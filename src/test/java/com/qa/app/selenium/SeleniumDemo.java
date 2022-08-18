71e50864660bfacd3cd0b8a24b9d8124b3e25423
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-08-18 15:12:55 +0100

    Selenium demo - waits

--- src/test/java/com/qa/app/selenium/SeleniumDemo.java
+++ src/test/java/com/qa/app/selenium/SeleniumDemo.java
@@ -1,5 +1,6 @@
 package com.qa.app.selenium;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
@@ -10,9 +11,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumDemo {
 
@@ -26,9 +32,12 @@ public class SeleniumDemo {
 		this.driver = new ChromeDriver();// linking our driver to chrome - expecting a chrome browser
 		this.driver.manage().window().maximize();// maximise the pop-up window
 		
-//		this.driver.manage().timeouts().implicitlyWait(5L, TimeUnit.SECONDS);//implicit wait
-	}
+//		this.driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);//implicit wait - global, so waits x amount time for the driver(webpage)
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(3000));
+		this.driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));//implicit wait
 
+	}
+	
 	@Test
 	public void testGoogle() {
 		this.driver.get("https://www.google.com/");
@@ -38,8 +47,28 @@ public class SeleniumDemo {
 		searchBar.sendKeys("disney");// type in "disney" into the search bar
 		searchBar.sendKeys(Keys.ENTER);// alternative: searchBar.submit(); - press enter
 		WebElement text = this.driver.findElement(By.cssSelector("#kp-wp-tab-overview > div.TzHB6b.cLjAic.LMRCfc > div > div > div > div > div > div:nth-child(4)"));
+		//explicit wait - for individual elements
+		new WebDriverWait(driver, Duration.ofMillis(5000)).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#kp-wp-tab-overview > div.TzHB6b.cLjAic.LMRCfc > div > div > div > div > div > div:nth-child(4)")));
+		//fluent wait--------------------           exits                              checks again                        element to find
+		new FluentWait<WebDriver>(driver).withTimeout(Duration.ofMillis(10000)).pollingEvery(Duration.ofMillis(5000)).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#kp-wp-tab-overview > div.TzHB6b.cLjAic.LMRCfc > div > div > div > div > div > div:nth-child(4)")));
+		
 		Assertions.assertTrue(text.getText().contains("16 October 1923"));
 	}
+	
+	@Test
+	public void testQA() {
+		this.driver.get("https://www.google.com/");
+		WebElement agreeBttn = this.driver.findElement(By.id("L2AGLb"));// finds the "Accept all" button by its id
+		agreeBttn.click();// Clicks the button
+		WebElement searchBar = this.driver.findElement(By.name("q"));// finds the search bar
+		searchBar.sendKeys("qa");// type in "disney" into the search bar
+		searchBar.sendKeys(Keys.ENTER);// alternative: searchBar.submit(); - press enter
+		WebElement qaLink = this.driver.findElement(By.className("yuRUbf"));
+		qaLink.click();
+		WebElement text = this.driver.findElement(By.cssSelector("body > div.site-container > footer > div.container > div.footer__inner > div.footer__right > p"));
+		Assertions.assertTrue(text.getText().contains("International House, 1 St Katharine’s Way, London, E1W 1UN"));
+	}
+	
 
 	@AfterEach//after
 	public void closeBrowser() {
